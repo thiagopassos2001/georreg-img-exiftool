0a19dadcdd9e08a084ea013a3f68b3a588a5c2ce
Thiago Passos <passos.thiago2001@gmail.com>
2025-05-02 08:47:32 -0300

    SetCoordExiftool and add examples

--- georref_img_exiftool.py
+++ georref_img_exiftool.py
@@ -3,13 +3,28 @@ import shutil
 import subprocess
 import os
 
+def SetCoordExiftool(file_path,lat_value,lon_value,exiftool_path="exiftool"):
+    """
+    Executa prompt "exiftool_path -GPSLatitude={lat_value}" -GPSLatitudeRef=S -GPSLongitude={lon_value} -GPSLongitudeRef=W file_path -P -overwrite_original
+    Não retorna valor
+    """
+
+    arg_parameter = [
+        exiftool_path,
+        f"-GPSLatitude={lat_value}","-GPSLatitudeRef=S",
+        f"-GPSLongitude={lon_value}","-GPSLongitudeRef=W",
+        file_path,"-P","-overwrite_original"]
+    
+    subprocess.run(arg_parameter)
+
+
 # Exiftool program
 exiftool_path = "exiftool\exiftool.exe"
 
 # Paths
-sheet_path = r"C:\Users\thiagop\Desktop\SINV_Placas_Crateus.xlsx"
-src_img_path = r'C:\Users\thiagop\Desktop\1_BLOCO_Images\1_BLOCO_Images'
-dst_img_path = r'C:\Users\thiagop\Desktop\Registro Fotográfico Georreferenciado'
+sheet_path = r"data\sheet\field_sheet.csv"
+src_img_path = r"data\src img"
+dst_img_path = r"data\dst img"
 
 # Sheet columns
 name_column = "FILE NAME"
@@ -17,7 +32,16 @@ lat_column = 'LAT'
 lon_column = 'LON'
 
 # Sheet and available photo
-df = pd.read_excel(sheet_path)
+df = pd.read_csv(sheet_path,encoding="latin9")
+# Process file
+df = df.rename(columns={
+    "2_7":"FILE NAME",
+})
+df[name_column] = df[name_column].apply(os.path.basename)
+df[lat_column] = df["2_1"].apply(lambda value:float(value.split(", ")[0]))
+df[lon_column] = df["2_1"].apply(lambda value:float(value.split(", ")[1]))
+
+# Get all files in src path
 available_files = os.listdir(src_img_path)
 
 # Match and filter
@@ -27,14 +51,11 @@ df = df[df[name_column].isin(available_files)]
 count = 0
 max_count = len(df)
 
-print(df)
-
 for index, row in df.iterrows():
     try:
         # Lat and lon value
         lat_value = abs(row[lat_column])
         lon_value = abs(row[lon_column])
-
         # Join paths
         file_name = row[name_column]
         src = os.path.join(src_img_path,file_name)
@@ -42,21 +63,8 @@ for index, row in df.iterrows():
 
         # Copy file
         shutil.copyfile(src,dst)
-
-        # Args
-        arg_parameter = [
-            exiftool_path,
-            f"-GPSLatitude={lat_value}",
-            "-GPSLatitudeRef=S",
-            f"-GPSLongitude={lon_value}",
-            "-GPSLongitudeRef=W",
-            dst,
-            "-P",
-            "-overwrite_original"
-            ]
-        
-        # Run
-        subprocess.run(arg_parameter)
+        # Set coords
+        SetCoordExiftool(dst,lat_value,lon_value,exiftool_path=exiftool_path)
     
     except Exception as e:
         print(e)
